9f20697ffc2e308d6d333feef0c9214ff85dd45d
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2024-08-28 10:27:10 -0300

    Delete operadoresAritmeticos directory

--- operadoresAritmeticos/associacao.py
+++
@@ -1,7 +0,0 @@
-# Associação ele verifica se ele está na sequência
-
-curso = "Curso de Pyton"
-print("Phyton" in curso) # True
-
-frutas = ['Maça', 'Banana', "Morango"]
-print("Uva" in frutas) # False
