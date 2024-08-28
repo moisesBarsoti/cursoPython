e6ed6a209cd166b5d308d46c7be3c15b6fe2fe57
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2024-08-27 22:47:17 -0300

    Subindo Arquivos

---
+++ stringEFatiamento/fatiamento.py
@@ -0,0 +1,9 @@
+name = "Moisés Barsoti Andrade de Oliveira"
+
+print(name[0]) # M
+print(name[:14]) # Moisés Barsoti
+print(name[10:]) # soti Andrade de Oliveira
+print(name[10:16]) # soti A
+print(name[10:16:2]) # st 
+print(name[:]) # Moisés Barsoti Andrade de Oliveira
+print(name[:: -1]) # Moisés Barsoti Andrade de Oliveira
