5f9c04ae8e9f2457b664b69840b4f6eebd4962d2
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2024-09-10 22:22:07 -0300

    Subindo Arquivos

--- projetos/sistemaBancarioPOO.py
+++ projetos/sistemaBancarioPOO.py
@@ -1,24 +1,34 @@
-class Cliente:
-    def __init__(self, nome, saldo, idade):
-        self.nome = nome
-        self.saldo = saldo
-        self.idade = idade
+class Conta:
+    def __init__(
+            self, 
+            saldo,
+            numero,
+            agencia,
+            cliente,
+            historico
+            ):
+        self._saldo = saldo
+        self._numero = numero
+        self._agencia = agencia
+        self._cliente = cliente
+        self._historico = historico
+
+    def saldo(self):
+        return f"{self.saldo}"
+    
+    def novaConta(self, cliente, numero):
+        self.cliente = cliente    
+        self.numero = numero    
 
     def sacar(self, valor):
-        
-        if self.saldo <= valor:
-            print(f"Você não pode sacar este valor de R${valor}")
-        else:
-            self.saldo -= valor 
+        if(self.saldo < valor):
+            print(f"Você não pode sacar este valor R${valor}")
+        else:    
+            self.saldo -= valor    
 
-    
     def depositar(self, valor):
-        self.saldo += valor
+        self.saldo += valor   
 
-    def __str__(self):
-        return f"Nome: {self.nome} Idade: {self.idade} Saldo: {self.saldo}"    
-
-cliente = Cliente("Moises", 100, 19)             
-cliente.depositar(100)
-cliente.sacar(300)
-print(cliente)
+class contaCorrente(Conta):
+    def limite(self,):
+        self.limite = 3
